9b8bdbf51c80a2b3bbd17b2ea61a0c7ea8bde087
qazsato <qazsato@gmail.com>
2019-04-20 07:47:36 +0900

    住所毎の集計関数追加

--- app/models/geo_address.rb
+++ app/models/geo_address.rb
@@ -17,4 +17,29 @@ class GeoAddress < ApplicationRecord
       features: features
     }
   end
+
+  def self.count_by_address_code(coordinates, level)
+    sql = ''
+    coordinates.each do |c|
+      sql += "UNION ALL\n" unless sql.empty?
+      sql += "SELECT GeomFromText('POINT(#{c[0]} #{c[1]})', 4326) point\n"
+    end
+
+    geo_addresses = self.find_by_sql("
+      SELECT geo_addresses.address_code, count(1) as count
+      FROM (#{sql}) as t
+      INNER JOIN geo_addresses
+      ON ST_Contains(geo_addresses.polygon, t.point) = 1
+      WHERE geo_addresses.level = #{level}
+      GROUP BY geo_addresses.address_code;")
+
+    count_by_address_codes = []
+    geo_addresses.each do |geo_address|
+      count_by_address_codes << {
+        address_code: geo_address.address_code,
+        count: geo_address.attributes['count']
+      }
+    end
+    count_by_address_codes
+  end
 end
