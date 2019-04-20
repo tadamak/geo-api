34934a0d179f35f3d74cadd0ab83062c1ff2faa0
qazsato <qazsato@gmail.com>
2019-04-21 07:38:59 +0900

    validation追加

--- app/controllers/v1/analytics/addresses_controller.rb
+++ app/controllers/v1/analytics/addresses_controller.rb
@@ -1,7 +1,20 @@
 class V1::Analytics::AddressesController < ApplicationController
+  before_action :validate_contains_params, only: [:contains]
+
   def contains
-    coordinates = JSON.parse(params[:coordinates])
+    # TODO: validation
+    coordinates = params[:coordinates]
+    coordinates = JSON.parse(coordinates) if coordinates.kind_of?(String)
     level = params[:level]
     @count = GeoAddress.count_by_address_code(coordinates, level)
+  end
+
+  private
+
+  def validate_contains_params
+    coordinates = params[:coordinates]
+    render status: :bad_request, json: { status: 400, message: 'Required coordinates.' } if coordinates.blank?
+    level = params[:level]
+    render status: :bad_request, json: { status: 400, message: 'Required level.' } if level.blank?
   end
 end
