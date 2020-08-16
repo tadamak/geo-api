e5a00782c3fde88fcbb4b5f61402fb4ed99d5be5
qazsato <qazsato@gmail.com>
2020-08-16 15:59:06 +0900

    解析数の上限を10000に引き上げ

--- app/models/geo_address.rb
+++ app/models/geo_address.rb
@@ -46,35 +46,39 @@ class GeoAddress < ApplicationRecord
     return geojsons
   end
 
-  def self.counts_by_address_code(locations, level)
-    sql = ''
-    locations.each do |l|
-      lat = l.split(',')[0]
-      lng = l.split(',')[1]
-      sql += "UNION ALL\n" unless sql.empty?
-      # NOTE: 都道府県のST_Containsが遅いため、最も下のレベル3(町丁・字等)で解析をする
-      sql += "SELECT address_code FROM geo_addresses WHERE (ST_Contains(polygon, ST_GEOMFROMTEXT('POINT(#{lng} #{lat})', 4326))) AND level = 3\n"
+  def self.counts_by_address_code(all_locations, level)
+    each_slice_num = 1000
+    slice_locations = all_locations.each_slice(each_slice_num).to_a
+    results = Parallel.map(slice_locations) do |locations|
+      sql = ''
+      locations.each do |l|
+        lat = l.split(',')[0]
+        lng = l.split(',')[1]
+        sql += "UNION ALL\n" unless sql.empty?
+        # NOTE: 都道府県のST_Containsが遅いため、最も下のレベル3(町丁・字等)で解析をする
+        sql += "SELECT address_code FROM geo_addresses WHERE (ST_Contains(polygon, ST_GEOMFROMTEXT('POINT(#{lng} #{lat})', 4326))) AND level = #{Address::LEVEL[:TOWN]}\n"
+      end
+      self.find_by_sql("SELECT t.address_code FROM (#{sql}) as t").pluck(:address_code)
     end
 
-    geo_addresses = self.find_by_sql("
-      SELECT t.address_code, count(*) as count
-      FROM (#{sql}) as t
-      GROUP BY t.address_code;")
+    address_codes = results.flatten
+    # 重複する住所コードでまとめて件数を取得する {address_code => count}
+    countResults = address_codes.group_by(&:itself).map{ |key, value| [key, value.count] }.to_h
 
     count = {}
-    geo_addresses.each do |geo_address|
-      code = Address.code_by_level(geo_address.address_code, level)
+    countResults.each do |key, value|
+      code = Address.code_by_level(key, level)
       if count[code].nil?
-        count[code] = geo_address.attributes['count']
+        count[code] = value
       elsif
-        count[code] += geo_address.attributes['count']
+        count[code] += value
       end
     end
 
     addresses = Address.where(code: count.keys)
 
     counts_by_address_code = []
-    count.map do |code, value|
+    count.each do |code, value|
       address = addresses.select{|a| a.code == code}.first
       counts_by_address_code << {
         address: address,
