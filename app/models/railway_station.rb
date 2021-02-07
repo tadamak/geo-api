89fb2234db0c421aafdac1cdfe847f04ada2da19
qazsato <qazsato@gmail.com>
2021-02-07 23:33:30 +0900

    緯度経度の不要コード削除

--- app/models/railway_station.rb
+++ app/models/railway_station.rb
@@ -2,7 +2,6 @@ class RailwayStation < ApplicationRecord
   include Swagger::StationSchema
 
   def location
-    return nil if latitude.nil? || longitude.nil?
     {
       lat: latitude,
       lng: longitude
