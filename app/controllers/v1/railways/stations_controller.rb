106acaf9fee95e2147501a84f31e1db1de03e73b
qazsato <qazsato@gmail.com>
2021-02-10 09:57:31 +0900

    GET /railways/stations パラメータ整理

--- app/controllers/v1/railways/stations_controller.rb
+++ app/controllers/v1/railways/stations_controller.rb
@@ -2,18 +2,15 @@ class V1::Railways::StationsController < ApplicationController
   include Swagger::RailwaysApi
 
   before_action :validate_page_params, only: [:index]
-  before_action :validate_index_params, only: [:index]
   before_action :validate_show_params, only: [:show]
 
   def index
     address_code = params[:address_code]
-    stations = RailwayStation.where('address_code LIKE ?', "#{address_code}%")
+    stations = RailwayStation
+    stations = stations.where('address_code LIKE ?', "#{address_code}%") if address_code.present?
 
     total = stations.count
-    offset = get_offset
-    limit = get_limit
-    stations = stations.offset(offset).limit(limit).order(address_code: :asc)
-
+    stations = stations.offset(get_offset).limit(get_limit).order(address_code: :asc)
     response.headers['X-Total-Count'] = total
     render json: stations
   end
@@ -23,13 +20,6 @@ class V1::Railways::StationsController < ApplicationController
   end
 
   private
-
-  def validate_index_params
-    address_code = params[:address_code]
-    if address_code.blank?
-      return render_400(ErrorCode::REQUIRED_PARAM, 'address_code の指定が必要です。')
-    end
-  end
 
   def validate_show_params
     code = params[:code]
