1651c68a9a36b03e1b56452ef06d817efb78dc44
qazsato <qazsato@gmail.com>
2021-02-10 22:17:08 +0900

    validation 追加

--- app/controllers/v1/schools_controller.rb
+++ app/controllers/v1/schools_controller.rb
@@ -22,8 +22,16 @@ class V1::SchoolsController < ApplicationController
 
   def validate_index_params
     address_code = params[:address_code]
+    school_type = params[:school_type]
+    school_admin = params[:school_admin]
     if address_code.present? && address_code.length != Address::CODE_DIGIT[:CITY]
       return render_400(ErrorCode::INVALID_PARAM, "address_code はレベル2(市区町村)を指定してください。")
+    end
+    if school_type.present? && !School.school_types.values.include?(school_type.to_i)
+      return render_400(ErrorCode::INVALID_PARAM, "誤った school_type を指定しています。")
+    end
+    if school_admin.present? && !School.school_admins.values.include?(school_admin.to_i)
+      return render_400(ErrorCode::INVALID_PARAM, "誤った school_admin を指定しています。")
     end
   end
 
