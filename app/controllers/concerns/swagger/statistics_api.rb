2a166e08226de5ca9d746e4fd6e923dbbd174cd4
qazsato <qazsato@gmail.com>
2021-02-23 11:23:09 +0900

    ドキュメント修正

--- app/controllers/concerns/swagger/statistics_api.rb
+++ app/controllers/concerns/swagger/statistics_api.rb
@@ -5,20 +5,20 @@ module Swagger::StatisticsApi
   included do
     swagger_path '/statistics/addresses/population' do
       operation :get do
-        key :description, '指定した住所コードの性別・年齢毎の人口数を取得します。'
+        key :description, '指定した住所コードの性別・年齢毎の人口を取得します。'
         key :tags, ['Statistics']
         security do
           key :access_token, []
         end
         parameter name: :address_code do
           key :in, :query
-          key :description, '住所コード'
+          key :description, '住所コード。カンマ区切りで複数指定可能。'
           key :required, false
           key :type, :string
         end
 
         response 200 do
-          key :description, '住所コード毎の件数'
+          key :description, '指定したコードの人口。複数指定時は合算値。'
           schema do
             key :'$ref', :AddressPopulation
           end
@@ -38,20 +38,20 @@ module Swagger::StatisticsApi
     swagger_path '/statistics/meshes/population' do
       operation :get do
         key :deprecated, true
-        key :description, '指定したメッシュコードの性別・年齢毎の人口数を取得します。'
+        key :description, '指定したメッシュコードの性別・年齢毎の人口を取得します。'
         key :tags, ['Statistics']
         security do
           key :access_token, []
         end
         parameter name: :address_code do
           key :in, :query
-          key :description, 'メッシュコード。10桁(4分の1地域メッシュ)まで指定可能。'
+          key :description, 'メッシュコード。10桁(4分の1地域メッシュ)まで指定可能。カンマ区切りで複数指定可能。'
           key :required, false
           key :type, :string
         end
 
         response 200 do
-          key :description, 'メッシュコード毎の件数'
+          key :description, '指定したコードの人口。複数指定時は合算値。'
           schema do
             key :required, [:male, :female]
             property :male do
